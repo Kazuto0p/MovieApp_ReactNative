a5b0d6ff7f4a48451b29e7f502be1d0e6081ed2c
umamahesh <umamahesh.exgratia@gmail.com>
2026-03-11 12:32:48 +0530

    config tailwind-3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,26 @@
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   // Include all files under `app/` (routes/layouts/components) so Tailwind scans them.
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./app.tsx",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
         primary: "#030014",
-        "secondary-color": "#FF6347",
+        secondary: "#151312",
+        light: {
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+        },
+        dark: {
+          100: '#221f3d',
+          200: '#0f0d23',
+      
+        },
+
         accent: "#AB8BFF",
-        GB: "#0dd514ff",
+
       },
     },
   },
